eb70934dfda8d01574be7e9b3bd0c9cf003fb282
unknown <benjaayala140@gmail.com>
2026-02-20 15:29:30 -0300

    Configure Cloudinary and Supabase for Strapi Production

--- admin/config/database.ts
+++ admin/config/database.ts
@@ -1,13 +1,31 @@
-export default ({ env }) => ({
-  connection: {
-    client: "postgres",
-    connection: {
-      host: env("DB_HOST", "127.0.0.1"),
-      port: env.int("DB_PORT", 5432),
-      database: env("DB_NAME", "mate_unico"),
-      user: env("DB_USER", "soporte"),
-      password: env("DB_PASSWORD", "123"),
-      ssl: false,
-    },
-  },
-});
+export default ({ env }) => {
+  const connectionUrl = env("DATABASE_URL");
+
+  let connectionConfig = {};
+
+  if (connectionUrl) {
+    connectionConfig = {
+      client: "postgres",
+      connection: {
+        connectionString: connectionUrl,
+        ssl: { rejectUnauthorized: false },
+      },
+    };
+  } else {
+    connectionConfig = {
+      client: "postgres",
+      connection: {
+        host: env("DB_HOST", "127.0.0.1"),
+        port: env.int("DB_PORT", 5432),
+        database: env("DB_NAME", "mate_unico"),
+        user: env("DB_USER", "soporte"),
+        password: env("DB_PASSWORD", "123"),
+        ssl: false,
+      },
+    };
+  }
+
+  return {
+    connection: connectionConfig,
+  };
+};
