e2b2560f7d5ca6244b3b33a13602e168b6a130a1
unknown <benjaayala140@gmail.com>
2026-02-20 17:00:47 -0300

    Fix Strapi v5 session middleware for Render proxy

--- admin/config/admin.ts
+++ admin/config/admin.ts
@@ -1,15 +1,6 @@
 export default ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
-    options: {
-      expiresIn: '7d',
-      cookie: {
-        secure: false,
-      },
-    },
-    events: {
-      onConnectionSuccess(e) { },
-    },
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
--- admin/config/middlewares.ts
+++ admin/config/middlewares.ts
@@ -1,12 +1,30 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
-  'strapi::session',
+  {
+    name: 'strapi::session',
+    config: {
+      secure: false, // Override to false since Render terminates SSL
+    },
+  },
   'strapi::favicon',
   'strapi::public',
 ];
